f1ce42e14e21dc3dcc30184138601c3b1c4d12d9
Tanya Wickham <TanyaKWickham@gmail.com>
2018-05-25 12:12:49 -0700

    uncommented code

--- app.js
+++ app.js
@@ -217,3 +217,57 @@ var alki = {
   },
 }; //Alki end
 alki.renderList();
+
+// // Starting Constructs
+// 'use strict';
+// // array that holds open hours for the stores
+// var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+// // array that holds the location objects
+// var allLocations = [];
+// // variable storing cookies from all stores
+// var totalTotal = 0;
+
+// function makeLocation(name, minCust, maxCust, avgCookies) {
+//   this.name = name;
+//   this.minCust = minCust;
+//   this.maxCust = maxCust;
+//   this.avgCookies = avgCookies;
+//   this.randomCustPerHour = [];
+//   this.avgCookiesSoldPerHour = [];
+//   this.totalCookies = 0;
+//   allLocations.push(this);
+//   //calc random customers by hour
+//   this.calcRanCustByHour = function () {
+//     for (var i = 0; i < hours.length; i++) {
+//       this.randomCustPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+//       console.log(this.randomCustPerHour[i]);
+//     }
+//   };
+//   //calc avg cookies sold per hour
+//   this.cookiesHour.push = function () {
+//     for (var j = 0; j < hours.length; j++) {
+      
+//     }
+//   }
+// }
+//using the this keyword in front of the method to show that the method belongs to the object that this function creates
+//this.calcRandCustByHour = function() {
+//methods go here
+//use prototypes
+// }
+
+// function newLocations() {
+//   new MakeLocation('First and Pike', 23, 65, 6.3);
+//   new MakeLocation('SeaTac Airport', 3, 24, 1.2);
+//   new MakeLocation('Seattle Center', 11, 38, 3.7);
+//   new MakeLocation('Capitol Hill', 20, 38, 2.3);
+//   new MakeLocation('Alki', 2, 16, 4.6);
+// };
+// //creating newLocations to populate into makeLocation that will push into allLocations
+// newLocations();
+
+// //making table
+// function createHeaderRow() {
+//   var nameOfLocation = document.getElementById('cookieTable');
+//   var thEl = document.createElement('th');
+// }
